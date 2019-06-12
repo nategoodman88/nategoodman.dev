d1a46dce8a2c00448067f01bad29e916037c3b6f
Nate Goodman <nategoodman88@gmail.com>
2019-06-12 13:01:34 -0400

    Complete restyle of page

---
+++ Jquery.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    $('div.hidden').fadeIn(1000).removeClass('hidden');
+}); 
+
+$(document).ready(function () {
+    $('div.hidden2').fadeIn(2000).removeClass('hidden2');
+});
+
+$(document).ready(function () {
+    $('div.hidden3').fadeIn(3000).removeClass('hidden3');
+});
+
+$(document).ready(function () {
+    $('.tab').fadeIn(4000).removeClass('.tab');
+});
