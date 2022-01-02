eaf76d8d455d32970a4eabeced3c9f0441f8976f
Nate Goodman <nategoodman88@gmail.com>
2022-01-02 14:50:26 -0500

    More design style changes and code cleanup

--- Jquery.js
+++ Jquery.js
@@ -1,15 +1,19 @@
-$(document).ready(function () {
+$(document).ready(function () 
+{
     $('div.hidden').fadeIn(1000).removeClass('hidden');
 }); 
 
-$(document).ready(function () {
+$(document).ready(function () 
+{
     $('div.hidden2').fadeIn(2000).removeClass('hidden2');
 });
 
-$(document).ready(function () {
+$(document).ready(function () 
+{
     $('div.hidden3').fadeIn(3000).removeClass('hidden3');
 });
 
-$(document).ready(function () {
+$(document).ready(function () 
+{
     $('.tab').fadeIn(4000).removeClass('.tab');
 });
