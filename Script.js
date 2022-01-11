ca0d70e4c58b010ed5899893869f537b00aa08b3
Nate Goodman <nategoodman88@gmail.com>
2022-01-11 14:50:19 -0500

    Add files via upload
    Added very simple dark mode script

---
+++ Script.js
@@ -0,0 +1,7 @@
+function swapMode() 
+{
+    // Call variable and set it to the initial body CSS
+    var element = document.body;
+    // Change the variable class to darkmode CSS
+    element.classList.toggle("darkmode");
+ }
