8514881fa8045c491d462644b7c5d1615b5d91f0
Nate Goodman <nategoodman88@gmail.com>
2023-11-13 11:59:25 -0500

    Update Script.js
    Removed script for api that no longer exists

--- Script.js
+++ Script.js
@@ -1,21 +1,3 @@
-//Function to use fetch; calls post to my api to update view count in my database
-function updateViewCount()
-{
-    fetch('https://nategoodmandevapi.herokuapp.com/views',
-        {"method":"POST"})
-    .then((response) => { 
-        if (response.status == 200) 
-        {
-            console.log("Success!")
-        }
-        else 
-        {
-            console.log("Error!")
-        }
-    })
-}
-//Function to update view count is called on load 
-window.onload = () => {updateViewCount()}
 //Checks local storage to see if user preference is saved to darkmode and switches to it if so
 if (localStorage.getItem("darkmode") == "true")
 {
